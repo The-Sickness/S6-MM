fb2a0712dda6047dec38da19ab11b36d808665b4
tuukka.tikkanen@linaro.org <tuukka.tikkanen@linaro.org>
2014-02-24 08:29:32 +0200

    cpuidle: Use actual state latency in menu governor
    Currently menu governor records the exit latency of the state it has
    chosen for the idle period. The stored latency value is then later
    used to calculate the actual length of the idle period. This value
    may however be incorrect, as the entered state may not be the one
    chosen by the governor. The entered state information is available,
    so we can use that to obtain the real exit latency.
    
    Signed-off-by: Tuukka Tikkanen <tuukka.tikkanen@linaro.org>
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: XileForce <rdaguiar360@gmail.com>
    
    Conflicts:
    	drivers/cpuidle/governors/menu.c

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -115,7 +115,6 @@ struct menu_device {
 
 	unsigned int	expected_us;
 	u64		predicted_us;
-	unsigned int	exit_us;
 	unsigned int	bucket;
 	u64		correction_factor[BUCKETS];
 	u32		intervals[INTERVALS];
@@ -281,7 +280,6 @@ static int menu_select(struct cpuidle_driver *drv, struct cpuidle_device *dev)
 	}
 
 	data->last_state_idx = 0;
-	data->exit_us = 0;
 
 	/* Special case when user has set very strict latency requirement */
 	if (unlikely(latency_req == 0))
@@ -352,7 +350,6 @@ static int menu_select(struct cpuidle_driver *drv, struct cpuidle_device *dev)
 			continue;
 
 		data->last_state_idx = i;
-		data->exit_us = s->exit_latency;
 	}
 
 	return data->last_state_idx;
@@ -403,8 +400,8 @@ static void menu_update(struct cpuidle_driver *drv, struct cpuidle_device *dev)
 	 * We correct for the exit latency; we are assuming here that the
 	 * exit latency happens after the event that we're interested in.
 	 */
-	if (measured_us > data->exit_us)
-		measured_us -= data->exit_us;
+	if (measured_us > target->exit_latency)
+		measured_us -= target->exit_latency;
 
 
 	/* update our correction ratio */
