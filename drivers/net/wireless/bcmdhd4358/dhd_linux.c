26e9640901ff60bf9888e2e79512da61b67667cb
HRTKernel <support@hacker-kernel.com>
2015-06-15 19:36:19 +0200

    reduce rx wakelock timeout via sysfs
    /sys/module/bcmdhd/parameters/wakelock_timeout
    
    Conflicts:
    	drivers/net/wireless/bcmdhd4358/dhd_linux.c

--- drivers/net/wireless/bcmdhd4358/dhd_linux.c
+++ drivers/net/wireless/bcmdhd4358/dhd_linux.c
@@ -83,6 +83,9 @@
 #include <dhd_dbg.h>
 #ifdef CONFIG_HAS_WAKELOCK
 #include <linux/wakelock.h>
+#include <linux/moduleparam.h>
+static int wakelock_timeout = 1;
+module_param(wakelock_timeout, int, 0755);
 #endif
 #ifdef WL_CFG80211
 #include <wl_cfg80211.h>
@@ -9265,6 +9268,10 @@ int dhd_os_wake_lock_timeout(dhd_pub_t *pub)
 		if (dhd->wakelock_ctrl_timeout_enable)
 			wake_lock_timeout(&dhd->wl_ctrlwake,
 				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable));
+				msecs_to_jiffies(dhd->wakelock_rx_timeout_enable)/wakelock_timeout);
+		if (dhd->wakelock_ctrl_timeout_enable)
+			wake_lock_timeout(&dhd->wl_ctrlwake,
+				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable)/wakelock_timeout);
 #endif
 		dhd->wakelock_rx_timeout_enable = 0;
 		dhd->wakelock_ctrl_timeout_enable = 0;
