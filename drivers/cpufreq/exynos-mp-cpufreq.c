8822312ca7fc0380de1337ce21b7fb818423bc3e
Andrei F <luxneb@gmail.com>
2015-08-04 00:34:28 +0200

    cpufreq: exynos-mp: Enforce stock frequency policies

--- drivers/cpufreq/exynos-mp-cpufreq.c
+++ drivers/cpufreq/exynos-mp-cpufreq.c
@@ -95,6 +95,10 @@
 		#define CL0_MAX_FREQ		1500000
 		#define CL1_MAX_FREQ		2100000
 	#endif
+#define CL0_MIN_FREQ		200000
+#define CL0_MAX_FREQ		1600000
+#define CL1_MIN_FREQ		200000
+#define CL1_MAX_FREQ		2300000
 #else
 #error "Please define core frequency ranges for current SoC."
 #endif
@@ -1150,6 +1154,7 @@ static struct notifier_block exynos_tmu_nb = {
 static int exynos_cpufreq_cpu_init(struct cpufreq_policy *policy)
 {
 	unsigned int cur = get_cur_cluster(policy->cpu);
+	int ret;
 
 	pr_debug("%s: cpu[%d]\n", __func__, policy->cpu);
 
@@ -1167,8 +1172,15 @@ static int exynos_cpufreq_cpu_init(struct cpufreq_policy *policy)
 		cpumask_copy(policy->cpus, &cluster_cpus[CL_ZERO]);
 		cpumask_copy(policy->related_cpus, &cluster_cpus[CL_ZERO]);
 	}
+	
+	ret = cpufreq_frequency_table_cpuinfo(policy, exynos_info[cur]->freq_table);
+	
+	if (!ret) {
+		policy->min = cur == CL_ONE ? CL1_MIN_FREQ : CL0_MIN_FREQ;
+		policy->max = cur == CL_ONE ? CL1_MAX_FREQ : CL0_MAX_FREQ;
+	}
 
-	return cpufreq_frequency_table_cpuinfo(policy, exynos_info[cur]->freq_table);
+	return ret;
 }
 
 static struct cpufreq_driver exynos_driver = {
