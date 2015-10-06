024aef72e309afcc4383a44e932eda01cd8dcd4e
googyanas <googyanas@gmail.com>
2015-10-06 22:14:14 +0100

    cpufreq changes

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -2465,7 +2465,7 @@ static
 struct cpufreq_governor cpufreq_gov_interactive = {
 	.name = "interactive",
 	.governor = cpufreq_governor_interactive,
-	.max_transition_latency = 10000000,
+	.max_transition_latency = 100000,
 	.owner = THIS_MODULE,
 };
 
