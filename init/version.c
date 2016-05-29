3193bcdc88b34e27202940745fab992417a3bd84
The-Sickness <alloycowboy6969@gmail.com>
2016-05-28 23:04:08 -0500

    Changed kernel author

--- init/version.c
+++ init/version.c
@@ -46,5 +46,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
+	" (The-Sickness)"
 	" (" LINUX_COMPILER ") %s\n";
