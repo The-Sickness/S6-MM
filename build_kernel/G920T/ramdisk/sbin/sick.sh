717dd60005890224efb795136d38dc216c2ec5f0
The-Sickness <alloycowboy6969@gmail.com>
2016-07-08 20:59:19 -0500

    Fixed permissions for small and big cores

--- build_kernel/G920T/ramdisk/sbin/sick.sh
+++ build_kernel/G920T/ramdisk/sbin/sick.sh
@@ -20,6 +20,80 @@ $BB mount -o remount,rw /system;
 setenforce 0;
 
 #####################################################################
+
+#####################################################################
+
+# VARS
+
+GOVLITTLE=/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+GOVBIG=/sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+FREQMINLITTLE1=/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+FREQMINLITTLE2=/sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+FREQMINLITTLE3=/sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+FREQMINLITTLE4=/sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
+FREQMAXLITTLE1=/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+FREQMAXLITTLE2=/sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+FREQMAXLITTLE3=/sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+FREQMAXLITTLE4=/sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+FREQMINBIG1=/sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+FREQMINBIG2=/sys/devices/system/cpu/cpu5/cpufreq/scaling_min_freq
+FREQMINBIG3=/sys/devices/system/cpu/cpu6/cpufreq/scaling_min_freq
+FREQMINBIG4=/sys/devices/system/cpu/cpu7/cpufreq/scaling_min_freq
+FREQMAXBIG1=/sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
+FREQMAXBIG2=/sys/devices/system/cpu/cpu5/cpufreq/scaling_max_freq
+FREQMAXBIG3=/sys/devices/system/cpu/cpu6/cpufreq/scaling_max_freq
+FREQMAXBIG4=/sys/devices/system/cpu/cpu7/cpufreq/scaling_max_freq
+SCHEDULER1=noop
+SCHEDULER2=sioplus
+SCHEDULER3=fiops
+SCHEDULER4=deadline
+SCHEDULER5=bfq
+SCHEDULER6=tripndroid
+SCHEDULER7=cfq
+SCHEDULER8=row
+CPUGOVERNOR1=alucard
+CPUGOVERNOR2=bioshock
+CPUGOVERNOR3=conservative
+CPUGOVERNOR4=conservativex
+CPUGOVERNOR5=dancedance
+CPUGOVERNOR6=darkness
+CPUGOVERNOR7=hyper
+CPUGOVERNOR8=interactive
+CPUGOVERNOR9=interextrem
+CPUGOVERNOR10=Lionheart
+CPUGOVERNOR11=nightmare
+CPUGOVERNOR12=ondemand
+CPUGOVERNOR13=ondemandplus
+CPUGOVERNOR14=performance
+CPUGOVERNOR15=preservative
+CPUGOVERNOR16=smartassV2
+CPUGOVERNOR17=userspace
+CPUGOVERNOR18=wheatley
+CPUFREQ1=200000
+CPUFREQ2=300000
+CPUFREQ3=400000
+CPUFREQ4=500000
+CPUFREQ5=600000
+CPUFREQ6=700000
+CPUFREQ7=800000
+CPUFREQ8=900000
+CPUFREQ9=1000000
+CPUFREQ10=1100000
+CPUFREQ11=1104000 #only A53 Cluster!
+CPUFREQ12=1200000
+CPUFREQ13=1296000 #only A53 Cluster!
+CPUFREQ14=1300000 #only A57 Cluster!
+CPUFREQ15=1400000
+CPUFREQ16=1500000
+CPUFREQ17=1600000
+CPUFREQ18=1704000 #only A57 Cluster!
+CPUFREQ19=1800000 #only A57 Cluster!
+CPUFREQ20=1896000 #only A57 Cluster!
+CPUFREQ21=2000000 #only A57 Cluster!
+CPUFREQ22=2100000 #only A57 Cluster!
+CPUFREQ23=2200000 #only A57 Cluster!
+CPUFREQ24=2304000 #only A57 Cluster!
+
 # Make Kernel Data Path
 
 if [ ! -d /data/.sickness ]; then
@@ -183,6 +257,204 @@ echo "2100000" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq;
 echo "2100000" > /sys/devices/system/cpu/cpu5/cpufreq/scaling_max_freq;
 echo "2100000" > /sys/devices/system/cpu/cpu6/cpufreq/scaling_max_freq;
 echo "2100000" > /sys/devices/system/cpu/cpu7/cpufreq/scaling_max_freq;
+
+#####################################################################
+
+# Assume SMP uses shared cpufreq policy for all CPUs
+chown root system $FREQMAXLITTLE1
+chmod 0644 $FREQMAXLITTLE1
+chown root system $FREQMINLITTLE1
+chmod 0644 $FREQMINLITTLE1
+chown root system $FREQMAXLITTLE2
+chmod 0644 $FREQMAXLITTLE2
+chown root system $FREQMINLITTLE2
+chmod 0644 $FREQMINLITTLE2
+chown root system $FREQMAXLITTLE3
+chmod 0644 $FREQMAXLITTLE3
+chown root system $FREQMINLITTLE3
+chmod 0644 $FREQMINLITTLE3
+chown root system $FREQMAXLITTLE4
+chmod 0644 $FREQMAXLITTLE4
+chown root system $FREQMINLITTLE4
+chmod 0644 $FREQMINLITTLE4
+chown root system $FREQMAXBIG1
+chmod 0644 $FREQMAXBIG1
+chown root system $FREQMINBIG1
+chmod 0644 $FREQMINBIG1
+chown root system $FREQMAXBIG2
+chmod 0644 $FREQMAXBIG2
+chown root system $FREQMINBIG2
+chmod 0644 $FREQMINBIG2
+chown root system $FREQMAXBIG3
+chmod 0644 $FREQMAXBIG3
+chown root system $FREQMINBIG3
+chmod 0644 $FREQMINBIG3
+chown root system $FREQMAXBIG4
+chmod 0644 $FREQMAXBIG4
+chown root system $FREQMINBIG4
+chmod 0644 $FREQMINBIG4
+
+#####################################################################
+
+#Setup Mhz Min/Max Cluster A53
+#echo $CPUFREQ16 > $FREQMAXLITTLE1
+#echo $CPUFREQ16 > $FREQMAXLITTLE2
+#echo $CPUFREQ16 > $FREQMAXLITTLE3
+#echo $CPUFREQ16 > $FREQMAXLITTLE4
+#Setup Mhz Min/Max Cluster A57
+#echo $CPUFREQ22 > $FREQMAXBIG1
+#echo $CPUFREQ22 > $FREQMAXBIG2
+#echo $CPUFREQ22 > $FREQMAXBIG3
+#echo $CPUFREQ22 > $FREQMAXBIG4
+
+#####################################################################
+
+# Parse CPU CLOCK from prop
+if [ "`grep "kernel.cpu.a53.min=200000" $PROP`" != "" ]; then
+	echo $CPUFREQ1 > $FREQMINLITTLE1
+	echo $CPUFREQ1 > $FREQMINLITTLE2
+	echo $CPUFREQ1 > $FREQMINLITTLE3
+	echo $CPUFREQ1 > $FREQMINLITTLE4
+elif [ "`grep "kernel.cpu.a53.min=300000" $PROP`" != "" ]; then
+	echo $CPUFREQ2 > $FREQMINLITTLE1
+	echo $CPUFREQ2 > $FREQMINLITTLE2
+	echo $CPUFREQ2 > $FREQMINLITTLE3
+	echo $CPUFREQ2 > $FREQMINLITTLE4
+elif [ "`grep "kernel.cpu.a53.min=400000" $PROP`" != "" ]; then
+	echo $CPUFREQ3 > $FREQMINLITTLE1
+	echo $CPUFREQ3 > $FREQMINLITTLE2
+	echo $CPUFREQ3 > $FREQMINLITTLE3
+	echo $CPUFREQ3 > $FREQMINLITTLE4
+else
+	echo $CPUFREQ3 > $FREQMINLITTLE1
+	echo $CPUFREQ3 > $FREQMINLITTLE2
+	echo $CPUFREQ3 > $FREQMINLITTLE3
+	echo $CPUFREQ3 > $FREQMINLITTLE4
+fi
+
+sleep 1;
+
+if [ "`grep "kernel.cpu.a53.max=1200000" $PROP`" != "" ]; then
+	echo $CPUFREQ12 > $FREQMAXLITTLE1
+	echo $CPUFREQ12 > $FREQMAXLITTLE2
+	echo $CPUFREQ12 > $FREQMAXLITTLE3
+	echo $CPUFREQ12 > $FREQMAXLITTLE4
+elif [ "`grep "kernel.cpu.a53.min=1296000" $PROP`" != "" ]; then
+	echo $CPUFREQ13 > $FREQMAXLITTLE1
+	echo $CPUFREQ13 > $FREQMAXLITTLE2
+	echo $CPUFREQ13 > $FREQMAXLITTLE3
+	echo $CPUFREQ13 > $FREQMAXLITTLE4
+elif [ "`grep "kernel.cpu.a53.min=1400000" $PROP`" != "" ]; then
+	echo $CPUFREQ15 > $FREQMAXLITTLE1
+	echo $CPUFREQ15 > $FREQMAXLITTLE2
+	echo $CPUFREQ15 > $FREQMAXLITTLE3
+	echo $CPUFREQ15 > $FREQMAXLITTLE4
+elif [ "`grep "kernel.cpu.a53.min=1500000" $PROP`" != "" ]; then
+	echo $CPUFREQ16 > $FREQMAXLITTLE1
+	echo $CPUFREQ16 > $FREQMAXLITTLE2
+	echo $CPUFREQ16 > $FREQMAXLITTLE3
+	echo $CPUFREQ16 > $FREQMAXLITTLE4
+elif [ "`grep "kernel.cpu.a53.min=1600000" $PROP`" != "" ]; then
+	echo $CPUFREQ17 > $FREQMAXLITTLE1
+	echo $CPUFREQ17 > $FREQMAXLITTLE2
+	echo $CPUFREQ17 > $FREQMAXLITTLE3
+	echo $CPUFREQ17 > $FREQMAXLITTLE4
+else
+	echo $CPUFREQ16 > $FREQMAXLITTLE1
+	echo $CPUFREQ16 > $FREQMAXLITTLE2
+	echo $CPUFREQ16 > $FREQMAXLITTLE3
+	echo $CPUFREQ16 > $FREQMAXLITTLE4
+fi
+
+sleep 1;
+
+if [ "`grep "kernel.cpu.a57.min=200000" $PROP`" != "" ]; then
+	echo $CPUFREQ1 > $FREQMINBIG1
+	echo $CPUFREQ1 > $FREQMINBIG2
+	echo $CPUFREQ1 > $FREQMINBIG3
+	echo $CPUFREQ1 > $FREQMINBIG4
+elif [ "`grep "kernel.cpu.a57.min=300000" $PROP`" != "" ]; then
+	echo $CPUFREQ2 > $FREQMINBIG1
+	echo $CPUFREQ2 > $FREQMINBIG2
+	echo $CPUFREQ2 > $FREQMINBIG3
+	echo $CPUFREQ2 > $FREQMINBIG4
+elif [ "`grep "kernel.cpu.a57.min=400000" $PROP`" != "" ]; then
+	echo $CPUFREQ3 > $FREQMINBIG1
+	echo $CPUFREQ3 > $FREQMINBIG2
+	echo $CPUFREQ3 > $FREQMINBIG3
+	echo $CPUFREQ3 > $FREQMINBIG4
+elif [ "`grep "kernel.cpu.a57.min=500000" $PROP`" != "" ]; then
+	echo $CPUFREQ4 > $FREQMINBIG1
+	echo $CPUFREQ4 > $FREQMINBIG2
+	echo $CPUFREQ4 > $FREQMINBIG3
+	echo $CPUFREQ4 > $FREQMINBIG4
+elif [ "`grep "kernel.cpu.a57.min=600000" $PROP`" != "" ]; then
+	echo $CPUFREQ5 > $FREQMINBIG1
+	echo $CPUFREQ5 > $FREQMINBIG2
+	echo $CPUFREQ5 > $FREQMINBIG3
+	echo $CPUFREQ5 > $FREQMINBIG4
+elif [ "`grep "kernel.cpu.a57.min=700000" $PROP`" != "" ]; then
+	echo $CPUFREQ6 > $FREQMINBIG1
+	echo $CPUFREQ6 > $FREQMINBIG2
+	echo $CPUFREQ6 > $FREQMINBIG3
+	echo $CPUFREQ6 > $FREQMINBIG4
+elif [ "`grep "kernel.cpu.a57.min=800000" $PROP`" != "" ]; then
+	echo $CPUFREQ7 > $FREQMINBIG1
+	echo $CPUFREQ7 > $FREQMINBIG2
+	echo $CPUFREQ7 > $FREQMINBIG3
+	echo $CPUFREQ7 > $FREQMINBIG4
+else
+	echo $CPUFREQ7 > $FREQMINBIG1
+	echo $CPUFREQ7 > $FREQMINBIG2
+	echo $CPUFREQ7 > $FREQMINBIG3
+	echo $CPUFREQ7 > $FREQMINBIG4
+fi
+
+sleep 1;
+
+if [ "`grep "kernel.cpu.a57.max=1704000" $PROP`" != "" ]; then
+	echo $CPUFREQ18 > $FREQMAXBIG1
+	echo $CPUFREQ18 > $FREQMAXBIG2
+	echo $CPUFREQ18 > $FREQMAXBIG3
+	echo $CPUFREQ18 > $FREQMAXBIG4
+elif [ "`grep "kernel.cpu.a57.max=1800000" $PROP`" != "" ]; then
+	echo $CPUFREQ19 > $FREQMAXBIG1
+	echo $CPUFREQ19 > $FREQMAXBIG2
+	echo $CPUFREQ19 > $FREQMAXBIG3
+	echo $CPUFREQ19 > $FREQMAXBIG4
+elif [ "`grep "kernel.cpu.a57.max=1896000" $PROP`" != "" ]; then
+	echo $CPUFREQ20 > $FREQMAXBIG1
+	echo $CPUFREQ20 > $FREQMAXBIG2
+	echo $CPUFREQ20 > $FREQMAXBIG3
+	echo $CPUFREQ20 > $FREQMAXBIG4
+elif [ "`grep "kernel.cpu.a57.max=2000000" $PROP`" != "" ]; then
+	echo $CPUFREQ21 > $FREQMAXBIG1
+	echo $CPUFREQ21 > $FREQMAXBIG2
+	echo $CPUFREQ21 > $FREQMAXBIG3
+	echo $CPUFREQ21 > $FREQMAXBIG4
+elif [ "`grep "kernel.cpu.a57.max=2100000" $PROP`" != "" ]; then
+	echo $CPUFREQ22 > $FREQMAXBIG1
+	echo $CPUFREQ22 > $FREQMAXBIG2
+	echo $CPUFREQ22 > $FREQMAXBIG3
+	echo $CPUFREQ22 > $FREQMAXBIG4
+elif [ "`grep "kernel.cpu.a57.max=2200000" $PROP`" != "" ]; then
+	echo $CPUFREQ23 > $FREQMAXBIG1
+	echo $CPUFREQ23 > $FREQMAXBIG2
+	echo $CPUFREQ23 > $FREQMAXBIG3
+	echo $CPUFREQ23 > $FREQMAXBIG4
+elif [ "`grep "kernel.cpu.a57.max=2304000" $PROP`" != "" ]; then
+	echo $CPUFREQ24 > $FREQMAXBIG1
+	echo $CPUFREQ24 > $FREQMAXBIG2
+	echo $CPUFREQ24 > $FREQMAXBIG3
+	echo $CPUFREQ24 > $FREQMAXBIG4
+else
+	echo $CPUFREQ22 > $FREQMAXBIG1
+	echo $CPUFREQ22 > $FREQMAXBIG2
+	echo $CPUFREQ22 > $FREQMAXBIG3
+	echo $CPUFREQ22 > $FREQMAXBIG4
+fi
+
+sleep 1;
 
 #####################################################################
 # Default DVFS Governor
